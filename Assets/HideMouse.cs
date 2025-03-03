1a9229e1664f837d0c83a21b582eba9ed5fa60b4
JDoig4 <justindoig6@gmail.com>
2025-03-02 22:54:37 -0800

    Added more coins and a cursor hiding feature

---
+++ Assets/HideMouse.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class HideMouse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
